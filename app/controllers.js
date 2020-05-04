8ddc33c71ee1740248f64468684e3771ccf86b5b
Matheus Brito <mfelippe.britto@gmail.com>
2020-05-04 20:09:33 -0300

    CRUD na Base de Dados

--- app/controllers.js
+++ app/controllers.js
@@ -5,13 +5,31 @@
 
    unidescApp.controller('produtoController', produtoController);
 
-   produtoController.$inject = ['$scope'];
+   produtoController.$inject = ['$scope', '$http'];
 
-   function produtoController($scope) {
+   function produtoController($scope, $http) {
       var index;
       var vm = this;
+
+      var HOST_HTTP = 'http://localhost:3333';
+
       vm.produto = {};
       vm.produtos = [];
+
+      vm.init = function () {
+         vm.listarProdutos();
+      };
+
+      vm.listarProdutos = function () {
+         $http.get(HOST_HTTP + '/produtos').then(
+            function (response) {
+               vm.produtos = response.data;
+            },
+            function (err) {
+               console.log(err)
+            }
+         );
+      };
 
       vm.adicionarProduto = function () {
 
@@ -21,20 +39,57 @@
          vm.limparCampos();
 
       };
+      vm.salvaProdutoBaseDados = function () {
+         $http.post(HOST_HTTP + 'produtos', vm.produto).then(
+            function (response) {
+               vm.produtos.push(vm.produto);
+               vm.limparCampos();
+               console.log(response);
+            },
+            function (err) {
+               console.log(err);
+            }
+         )
+      };
+
       vm.limparCampos = function () {
          vm.produto = {};
       };
       vm.delProdTab = function () {
          vm.produtos.splice(vm.produtos.indexOf(vm.produtod));
+         vm.deletarProdutoBaseDeDados();
       };
+
+      vm.deletarProdutoBaseDeDados = function () {
+         $http.delete(HOST_HTTP + '/produtos', vm.produto.id).then(
+            function (response) {
+               console.log(response);
+               vm.produtos.splice(item, 1);
+            },
+            function (err) {
+               console.log(err);
+            }
+         );
+      };
+
       vm.updateProdTab = function (item, i) {
          $scope.update = angular.copy(item);
          index = i;
-
+         vm.atualizarProdutoBaseDeDados();
       };
       vm.saveUpdate = function () {
          $scope.vm.produtos[index] = $scope.update;
          $scope.update = "";
+      };
+      vm.atualizarProdutoBaseDeDados = function () {
+         $http.put(HOST_HTTP + '/produtos', vm.produto.id, vm.produto).then(
+            function (response) {
+               console.log(response)
+            },
+            function (err) {
+               console.log(err);
+            }
+         )
       };
    }
 
